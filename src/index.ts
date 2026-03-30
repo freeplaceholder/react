6fbc672aea7fff22e0c0b2886ca08cbf3a3b6d8f
Camden Conekin <48098857+cconekin@users.noreply.github.com>
2026-03-30 10:58:39 -0400

    feat: Added snippet scaffolding

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,33 @@
 export { Placeholder } from "./placeholder";
 export { Avatar } from "./avatar";
-export { placeholderUrl, avatarUrl, configure, getConfig, toPlaceholderOptions, toAvatarOptions } from "@freeplaceholder/core";
+export {
+  avatarUrl,
+  configure,
+  DEFAULT_SNIPPET_HEIGHT,
+  DEFAULT_SNIPPET_WIDTH,
+  getConfig,
+  placeholderUrl,
+  snippetUrl,
+  toAvatarOptions,
+  toPlaceholderOptions,
+} from "@freeplaceholder/core";
 
 export type { PlaceholderProps } from "./placeholder";
 export type { AvatarProps } from "./avatar";
 export type {
+  AvatarComponentProps,
+  AvatarOptions,
   BorderStyle,
   FontWeight,
+  FreePlaceholderConfig,
   GradientDirection,
   ImageFormat,
   LetterSpacing,
+  PlaceholderComponentProps,
+  PlaceholderOptions,
   Rounded,
+  SnippetOptions,
   TextAlign,
   TextDecoration,
   TextTransform,
-  PlaceholderOptions,
-  AvatarOptions,
-  PlaceholderComponentProps,
-  AvatarComponentProps,
-  FreePlaceholderConfig,
 } from "@freeplaceholder/core";
